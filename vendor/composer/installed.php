fd5aef08240cbec1fbffd14d2b3c7f2b250e38aa
Carlos Andre Oliveira Pereira <carlos12andre13@gmail.com>
2025-10-12 00:57:04 -0300

    fd

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'assetmanager/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cfc49a586343ebd1c9da38fe3bdf219d2d90c705',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'assetmanager/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cfc49a586343ebd1c9da38fe3bdf219d2d90c705',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
